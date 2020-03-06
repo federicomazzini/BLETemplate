9145851ee62c160f26afee5adc94f32c8546eb04
Federico Mazzini <federico.mazzini@externos.bci.cl>
2020-03-05 22:21:09 -0300

    Initial Commit

---
+++ BLEThing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BLEThing
+//
+//  Created by Nisum on 05-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
